abbf9c5e1ce8aaf5027695a273ebf20f8bc9962f
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-01-31 18:43:18 -0600

    Inclusion of assets in constants

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,19 @@
+import twentyOnePilotsCover from './assets/covers/twenty-one-pilots.png'
+import regionalAtBestCover from './assets/covers/regional-at-best.png'
+import vesselCover from './assets/covers/vessel.png'
+import blurryFaceCover from './assets/covers/blurryface.png'
+import trenchCover from './assets/covers/trench.png'
+import scaledAndIcyCover from './assets/covers/scaled-and-icy.png'
+import clancyCover from './assets/covers/clancy.png'
+import tylerPhoto from './assets/members/tyler.png'
+import joshPhoto from './assets/members/josh.png'
+
 export const ALBUMS = [
    {
     id: 1,
     name: "Twenty One Pilots",
     release: 2009,
-    urlCover: "https://upload.wikimedia.org/wikipedia/en/a/af/Twenty_One_Pilots_album_cover.png",
+    urlCover: twentyOnePilotsCover,
     description: "Álbum debut de la banda, lanzado de manera independiente. Contiene temas introspectivos y una mezcla de estilos.",
     color: "#55732E",
     tracks: [
@@ -97,7 +107,7 @@ export const ALBUMS = [
     id: 2,
     name: "Regional at Best",
     release: 2011,
-    urlCover: "https://upload.wikimedia.org/wikipedia/en/3/3d/Regional_at_Best.png",
+    urlCover: regionalAtBestCover,
     description: "Un álbum conceptual que sentó las bases del sonido característico de la banda. Contiene varias canciones que luego serían regrabadas para álbumes futuros.",
     color: "#A8DADC",
     tracks: [
@@ -191,7 +201,7 @@ export const ALBUMS = [
     id: 3,
     name: "Vessel",
     release: 2013,
-    urlCover: "https://upload.wikimedia.org/wikipedia/en/0/0f/Twenty_One_Pilots_-_Vessel.png",
+    urlCover: vesselCover,
     description: "Primer álbum bajo el sello Fueled by Ramen. Combina letras profundas con elementos electrónicos y alternativos.",
     color: "#6B7A8F",
     tracks: [
@@ -273,7 +283,7 @@ export const ALBUMS = [
     id: 4,
     name: "Blurryface",
     release: 2015,
-    urlCover: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Blurryface_by_Twenty_One_Pilots.png/220px-Blurryface_by_Twenty_One_Pilots.png",
+    urlCover: blurryFaceCover,
     description: "Álbum icónico que consolidó a la banda en la escena musical global. Contiene éxitos como 'Stressed Out' y 'Ride'.",
     color: "#741b1b",
     tracks: [
@@ -367,7 +377,7 @@ export const ALBUMS = [
     id: 5,
     name: "Trench",
     release: 2018,
-    urlCover: "https://upload.wikimedia.org/wikipedia/en/f/f3/Trench_Twenty_One_Pilots.png",
+    urlCover: trenchCover,
     description: "Un álbum conceptual que explora temas de lucha interna y resistencia. Incluye canciones como 'Jumpsuit' y 'My Blood'.",
     color: "#eeca3c",
     tracks: [
@@ -461,7 +471,7 @@ export const ALBUMS = [
     id: 6,
     name: "Scaled and Icy",
     release: 2021,
-    urlCover: "https://upload.wikimedia.org/wikipedia/en/thumb/5/52/Twenty_One_Pilots_-_Scaled_and_Icy.png/220px-Twenty_One_Pilots_-_Scaled_and_Icy.png",
+    urlCover: scaledAndIcyCover,
     description: "Un álbum más optimista y colorido, lanzado en medio de la pandemia. Representa un contraste con el tono oscuro de álbumes previos.",
     color: "#f07ba6",
     tracks: [
@@ -537,7 +547,7 @@ export const ALBUMS = [
     id: 7,
     name: "Clancy",
     release: 2024,
-    urlCover: "https://upload.wikimedia.org/wikipedia/en/d/db/Twenty_One_Pilots_-_Clancy.png",
+    urlCover: clancyCover,
     description: "",
     color: "#d33a35",
     tracks: [
@@ -629,13 +639,13 @@ export const MEMBERS = {
     role: "Vocalist, pianist, composer, bassist",
     age: 36,
     description: "Tyler Joseph is the lead vocalist, pianist, and primary songwriter of Twenty One Pilots. As the band's creative mastermind, Tyler blends introspective and thought-provoking lyrics with captivating melodies. His ability to craft complex narratives, such as the Trench universe, sets the band apart. Known for his raw vulnerability, Tyler connects deeply with fans through themes of mental health, identity, and hope. His performances are marked by emotional intensity, making him a defining voice in alternative music.",
-    image: "https://images.genius.com/477f70a837da521e3d5e83c8c0609e1d.906x906x1.jpg"
+    image: tylerPhoto
   },
   josh: {
     name: "Josh Dun",
     role: "Drummer, percussionist",
     age: 37,
     description: "Josh Dun is the dynamic drummer and percussionist of Twenty One Pilots. Joining the band in 2011, he quickly became integral to its identity, bringing energy and charisma to both recordings and live shows. Renowned for his powerful drumming and vibrant stage presence, Josh complements Tyler's introspective style with rhythmic precision. His creativity extends beyond percussion, contributing to the band’s unique sound. Together, Josh and Tyler form a duo celebrated for pushing the boundaries of modern music.",
-    image: "	https://images.genius.com/f16c1949d7550087486472c95ccf90da.1000x1000x1.jpg"
+    image: joshPhoto
   }
 };
