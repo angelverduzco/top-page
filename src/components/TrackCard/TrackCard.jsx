f0ee1097b342b1c8e7c397b964e9b75814cb1b15
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-02-05 18:26:37 -0600

    Tracklist and TrackCard components

---
+++ src/components/TrackCard/TrackCard.jsx
@@ -0,0 +1,19 @@
+import './TrackCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify, faApple } from '@fortawesome/free-brands-svg-icons';
+
+export function TrackCard({ track }) { 
+    return (
+        <div className='track-card'>
+            <div className='track-card-info'>
+                <span className='track-card-number'>{track.id}</span>
+                <h3 className='track-card-title'>{track.title}</h3>
+            </div>
+            <div>
+                <a className='track-card-link' href={ track.spotify }><FontAwesomeIcon className='track-card-icon' icon={faSpotify} size='lg' /></a>
+                {track.appleMusic ? <a className='track-card-link' href={ track.appleMusic }><FontAwesomeIcon className='track-card-icon' icon={faApple} size='lg' /></a> : null }
+                <span className='track-card-duration'>{track.duration}</span>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Tracklist/Tracklist.jsx
@@ -0,0 +1,11 @@
+import './Tracklist.css';
+import { TrackCard } from '../TrackCard/TrackCard';
+
+export function Tracklist({ tracks }) { 
+    return (
+        <section className='tracklist-section'>
+            <h2 className='tracklist-title'>Tracklist</h2>
+            {tracks.map(track => <TrackCard key={track.id} track={ track } />)}
+        </section>
+    )
+}
