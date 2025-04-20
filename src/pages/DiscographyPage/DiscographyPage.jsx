65b3f36e39235f812c287a6fc9b7bc95a4d7f624
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-04-20 13:56:22 -0600

    Created DiscographySection and DiscographyPage

--- src/pages/DiscographyPage/DiscographyPage.jsx
+++ src/pages/DiscographyPage/DiscographyPage.jsx
@@ -1,5 +1,27 @@
 import "./DiscographyPage.css";
+import { ALBUMS, LIVE_ALBUMS, EXTENDED_PLAYS, SINGLES } from "../../data";
+import DiscographySection from "../../components/DiscographySection/DiscographySection";
 
 export function DiscographyPage() {
-    return <h1>To be created</h1>;
+    const SECTIONS = [
+        { section: "Albums", elements: ALBUMS },
+        { section: "Live Albums", elements: LIVE_ALBUMS },
+        { section: "Extended Plays", elements: EXTENDED_PLAYS },
+        { section: "Singles", elements: SINGLES },
+    ];
+
+    return (
+        <main className="discography-page">
+            <h1 className="discography-title">
+                Twenty One Pilots&apos; Discography
+            </h1>
+            {SECTIONS.map(({ section, elements }, index) => (
+                <DiscographySection
+                    key={index}
+                    section={section}
+                    elements={elements}
+                />
+            ))}
+        </main>
+    );
 }
