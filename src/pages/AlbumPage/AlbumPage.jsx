5201f696c54ff2b8f233128332f4ab2e5017620a
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-04-20 13:57:37 -0600

    Added logic of using params from the URL

--- src/pages/AlbumPage/AlbumPage.jsx
+++ src/pages/AlbumPage/AlbumPage.jsx
@@ -1,13 +1,29 @@
 import { AlbumBanner } from "../../components/AlbumBanner/AlbumBanner";
 import { useParams } from "react-router";
-import { ALBUMS } from "../../data";
+import { ALBUMS, LIVE_ALBUMS, SINGLES, EXTENDED_PLAYS } from "../../data";
 import "./AlbumPage.css";
 import { Tracklist } from "../../components/Tracklist/Tracklist";
 import { AlbumOptions } from "../../components/AlbumOptions/AlbumOptions";
 
 export function AlbumPage() {
-    const { albumId } = useParams();
-    const album = ALBUMS.find((a) => a.id === Number(albumId));
+    const { type, id } = useParams();
+
+    // Mapeo de tipos a datos
+    const dataMap = {
+        albums: ALBUMS,
+        "live-albums": LIVE_ALBUMS,
+        "extended-plays": EXTENDED_PLAYS,
+        singles: SINGLES,
+    };
+
+    // Obtener el conjunto de datos correspondiente
+    const dataset = dataMap[type] || [];
+    const album = dataset.find((a) => a.id === Number(id));
+
+    // Manejo de errores si no se encuentra el álbum
+    if (!album) {
+        return <p>Album not found</p>;
+    }
 
     return (
         <>
@@ -15,7 +31,7 @@ export function AlbumPage() {
             <main className="album-page">
                 <div className="album-page-content">
                     <section className="about-section">
-                        <h2 className="about-section-title">About the album</h2>
+                        <h2 className="about-section-title">About it</h2>
                         <p className="about-section-desc">
                             {album.description}
                         </p>
