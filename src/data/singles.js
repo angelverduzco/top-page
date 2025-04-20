cc0aee695e342db72221441aa2da2aa3e51cf852
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-04-20 13:49:37 -0600

    Converted images from PNG to WEBP

--- src/data/singles.js
+++ src/data/singles.js
@@ -1,32 +1,197 @@
 export const SINGLES = [
     {
         id: 1,
-        name: "Heathens",
+        name: "Cancer",
         release: 2016,
-        cover: "/covers/singles/heathens.png",
+        cover: "/covers/singles/cancer.webp",
+        description:
+            "Twenty One Pilots' rendition of My Chemical Romance's \"Cancer\" was released in 2016 as part of the tribute album Rock Sound Presents: The Black Parade. The duo reimagines the original's somber tone with minimalist instrumentation and layered vocals, adding electronic elements that enhance the song's emotional depth. Their interpretation maintains the poignant essence of the original while infusing it with their signature style, offering a fresh perspective on the themes of illness and farewell.",
+        color: "#591914",
+        links: {
+            spotify:
+                "https://open.spotify.com/intl-es/album/22uQ3YSLjQJPNPkZQzdX2y?si=0SmksbHqSie4Wr-VxqvSsw",
+            appleMusic:
+                "https://music.apple.com/mx/album/cancer-single/1152679154",
+        },
+        tracks: [
+            {
+                id: 1,
+                title: "Cancer",
+                duration: "3:56",
+                links: {
+                    spotify:
+                        "https://open.spotify.com/intl-es/album/22uQ3YSLjQJPNPkZQzdX2y?si=0SmksbHqSie4Wr-VxqvSsw",
+                    appleMusic:
+                        "https://music.apple.com/mx/album/cancer-single/1152679154",
+                },
+            },
+        ],
     },
     {
         id: 2,
-        name: "Level of Concern",
-        release: 2020,
-        covers: "/covers/singles/level-of-concern.png",
+        name: "Heathens",
+        release: 2016,
+        cover: "/covers/singles/heathens.webp",
+        description:
+            'Released on June 16, 2016, "Heathens" served as the lead single for the Suicide Squad movie soundtrack. Blending alternative hip hop and rock, the track delves into themes of outsider identity and caution. Its haunting melody and lyrics align with the film\'s dark aesthetic. "Heathens" peaked at number two on the US Billboard Hot 100 and earned three Grammy nominations. The accompanying music video features the band performing in a prison setting interspersed with scenes from the movie.',
+        color: "#D90404",
+        links: {
+            spotify:
+                "https://open.spotify.com/intl-es/album/3J8W9AOjQhnBLCX33m3atT?si=JJ3DUnXcSzKX2VE6lFSiCA",
+            appleMusic: "https://music.apple.com/mx/song/heathens/1125281487",
+        },
+        tracks: [
+            {
+                id: 1,
+                title: "Heathens",
+                duration: "3:15",
+                links: {
+                    spotify:
+                        "https://open.spotify.com/intl-es/album/3J8W9AOjQhnBLCX33m3atT?si=JJ3DUnXcSzKX2VE6lFSiCA",
+                    appleMusic:
+                        "https://music.apple.com/mx/song/heathens/1125281487",
+                },
+            },
+        ],
     },
     {
         id: 3,
-        name: "Christmas Saves the Year",
+        name: "Level of Concern",
         release: 2020,
-        cover: "/covers/singles/christmas-saves-the-year.png",
+        cover: "/covers/singles/level-of-concern.webp",
+        description:
+            '"Level of Concern" was released on April 9, 2020, as a standalone single reflecting the uncertainties of the COVID-19 pandemic. Departing from the band\'s conceptual narratives, the song offers a hopeful message amidst global turmoil. Its upbeat, dance-pop sound marked a stylistic shift, resonating with listeners seeking comfort. The track peaked at number 23 on the Billboard Hot 100 and was praised for being a timely anthem during unprecedented times.',
+        color: "#F2E527",
+        links: {
+            spotify:
+                "https://open.spotify.com/intl-es/album/4h3HXlnt6lryGzGbWmcFuY?si=umvDETTsSwqzzuGTDmiOnA",
+            appleMusic:
+                "https://music.apple.com/mx/album/level-of-concern-single/1507187367",
+        },
+        tracks: [
+            {
+                id: 1,
+                title: "Level of Concern",
+                duration: "3:40",
+                links: {
+                    spotify:
+                        "https://open.spotify.com/intl-es/album/4h3HXlnt6lryGzGbWmcFuY?si=umvDETTsSwqzzuGTDmiOnA",
+                    appleMusic:
+                        "https://music.apple.com/mx/album/level-of-concern-single/1507187367",
+                },
+            },
+        ],
     },
     {
         id: 4,
-        name: "The Craving - Single Version",
-        release: 2024,
-        cover: "/covers/singles/the-craving-single-version.png",
+        name: "Christmas Saves the Year",
+        release: 2020,
+        cover: "/covers/singles/christmas-saves-the-year.webp",
+        description:
+            'Released on December 8, 2020, "Christmas Saves the Year" is a lo-fi holiday single written and recorded by Tyler Joseph in his home studio. Inspired by the challenges of 2020, the song emphasizes finding joy and hope during the festive season. Its mellow instrumentation and reflective lyrics offer solace, highlighting the importance of togetherness. The track debuted during a charity Twitch stream and was accompanied by a claymation-style music video. ',
+        color: "#D94E41",
+        links: {
+            spotify:
+                "https://open.spotify.com/intl-es/album/5bh5kgZddiyd5ObXOP9Exu?si=LJRN44hqTnCjTfsLIW_qoQ",
+            appleMusic:
+                "https://music.apple.com/mx/album/christmas-saves-the-year-single/1543103935",
+        },
+        tracks: [
+            {
+                id: 1,
+                title: "Christmas Saves the Year",
+                duration: "3:32",
+                links: {
+                    spotify:
+                        "https://open.spotify.com/intl-es/album/5bh5kgZddiyd5ObXOP9Exu?si=LJRN44hqTnCjTfsLIW_qoQ",
+                    appleMusic:
+                        "https://music.apple.com/mx/album/christmas-saves-the-year-single/1543103935",
+                },
+            },
+        ],
     },
     {
         id: 5,
+        name: "The Craving - Single Version",
+        release: 2024,
+        cover: "/covers/singles/the-craving.webp",
+        description:
+            "\"The Craving - Single Version\" is a track from the 2024 album Clancy. This version presents a more stripped-down arrangement compared to the album cut, emphasizing raw emotion and vulnerability. The song explores themes of desire and internal conflict, aligning with the album's overarching narrative. Its introspective lyrics and minimalist production showcase the band's ability to convey profound messages through simplicity.",
+        color: "#D33A35",
+        links: {
+            spotify:
+                "https://open.spotify.com/intl-es/album/7K7eWZNR5t9UBlrN2ltzLI?si=ZQ5j59thTWuXWsOjr7e5nw",
+            appleMusic:
+                "https://music.apple.com/mx/album/the-craving-single-version-single/1747337609",
+        },
+        tracks: [
+            {
+                id: 1,
+                title: "The Craving - Single Version",
+                duration: "2:53",
+                links: {
+                    spotify:
+                        "https://open.spotify.com/intl-es/album/7K7eWZNR5t9UBlrN2ltzLI?si=ZQ5j59thTWuXWsOjr7e5nw",
+                    appleMusic:
+                        "https://music.apple.com/mx/album/the-craving-single-version-single/1747337609",
+                },
+            },
+        ],
+    },
+    {
+        id: 6,
         name: "The Line (from the series Arcane League Of Legends)",
         release: 2024,
-        cover: "/covers/singles/the-line.png",
+        cover: "/covers/singles/the-line.webp",
+        description:
+            'Featured on the Clancy album, "The Line" delves into the concept of confronting personal boundaries and the fear associated with crossing them. The lyrics portray a struggle with self-doubt and the longing for support during transformative moments. Musically, the track combines melancholic melodies with subtle electronic elements, creating an atmosphere of introspection. It\'s a poignant reflection on growth, vulnerability, and the courage to face the unknown.',
+        color: "#074E8C",
+        links: {
+            spotify:
+                "https://open.spotify.com/album/4aE8ZrEif8MgWvFNIbz8I6?si=imPnVnHoRsyu2xP0WElw1A",
+            appleMusic:
+                "https://music.apple.com/mx/song/the-line-from-the-series-arcane-league-of-legends/1767032205",
+        },
+        tracks: [
+            {
+                id: 1,
+                title: "The Line (from the series Arcane League Of Legends)",
+                duration: "3:25",
+                links: {
+                    spotify:
+                        "https://open.spotify.com/album/4aE8ZrEif8MgWvFNIbz8I6?si=imPnVnHoRsyu2xP0WElw1A",
+                    appleMusic:
+                        "https://music.apple.com/mx/song/the-line-from-the-series-arcane-league-of-legends/1767032205",
+                },
+            },
+        ],
+    },
+    {
+        id: 7,
+        name: "Doubt (Demo)",
+        release: 2025,
+        cover: "/covers/singles/doubt.webp",
+        description:
+            "\"Doubt (Demo)\" is a raw and unpolished version of the track from the 2015 album Blurryface. This demo showcases the band's creative process, featuring stripped-down instrumentation and Tyler Joseph's emotive vocals. The song explores themes of uncertainty and introspection, allowing listeners to connect with the band on a more personal level. Its candid nature provides insight into the evolution of the final version, highlighting the band's artistic journey.",
+        color: "#D95043",
+        links: {
+            spotify:
+                "https://open.spotify.com/album/5qkGe4UpUa8dsokzuroQ72?si=UndeI8XXTW-98jcE9ei2RQ",
+            appleMusic:
+                "https://music.apple.com/us/album/doubt-demo-single/1807576690",
+        },
+        tracks: [
+            {
+                id: 1,
+                title: "Doubt (Demo)",
+                duration: "3:15",
+                links: {
+                    spotify:
+                        "https://open.spotify.com/album/5qkGe4UpUa8dsokzuroQ72?si=UndeI8XXTW-98jcE9ei2RQ",
+                    appleMusic:
+                        "https://music.apple.com/us/album/doubt-demo-single/1807576690",
+                },
+            },
+        ],
     },
 ];
