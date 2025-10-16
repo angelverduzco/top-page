525e3bf96cdcfbdae947703d5932ca2064971a21
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-10-15 20:10:54 -0600

    Changed banner image and active color of nav elements

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,16 +2,13 @@ import "./Banner.css";
 import banner from "../../assets/banner.png";
 
 export function Banner() {
-    return (
-        <section
-            className="banner"
-            style={{ backgroundImage: `url(${banner})` }}
-        >
-            <h1 className="banner-title">Welcome to the Resistance</h1>
-            <p className="banner-text">
-                Together, we push on through. Welcome to a world where the
-                Banditos thrive.
-            </p>
-        </section>
-    );
+  return (
+    <section className="banner" style={{ backgroundImage: `url(${banner})` }}>
+      <h1 className="banner-title">Welcome to the Resistance</h1>
+      <p className="banner-text">
+        Together, we push on through. Welcome to a world where the Banditos
+        thrive.
+      </p>
+    </section>
+  );
 }
